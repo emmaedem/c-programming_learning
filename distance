2892f894dce6bce5319dcf26d770fad4eb64e35d
emmaedem <emmaontop86@yahoo.com>
2023-02-16 19:49:48 +0100

    update

---
+++ distance
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int speed = 0;
+	int time = 0;
+
+	printf('speed: ");
+	scanf("%d", &speed); 
+
+	printf('time: ");
+        scanf("%d", &time);
+
+	int distance = speed * time:
+	printf(distance: %d\n", distance);
+
+	return (0);
+}i
---
+++ raius.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+
+{
+	int radius: // The distance from the outside of center of the circle
+	printf("please enter a radius: ");
+
+	scanf("%d", &radius); // address of operator
+	
+	int area + 3.14159 * (radius * radius);
+
+	printf("%f\n" radius);
+
+	return (0);
+}
